43ead01f4b569cb64cfc8040f41e1f273fdc705a
Augustin Borsu <a.borsu@gmail.com>
2015-10-16 10:11:24 +0200

    Add automount NixDmg to .profile
    The created nix store is now automounted if it exists.

--- install-nix.sh
+++ install-nix.sh
@@ -1,20 +1,29 @@
 #!/bin/bash -e
 
-DIST="`pwd`"
 DMG=~/NixStore.dmg
 
 echo "Creating disk image"
 hdiutil create -size 8G -fs "Case-sensitive HFS+" -volname NixStore $DMG
 sudo mkdir -p /nix
 sudo chown $USER /nix
-./attach-disk.sh
+hdiutil attach $DMG -mountpoint /nix
 
 echo "Downloading and installing Nix"
-sudo chown -R $USER /nix
 curl https://nixos.org/nix/install | sh
 
-echo "Adding environment variables to ~/.profile"
 RCFILE=~/.profile
-echo "source ~/.nix-profile/etc/profile.d/nix.sh" >> $RCFILE
+echo "Adding environment variables to $RCFILE"
+echo "
+if [ -e ~/.nix-profile/etc/profile.d/nix.sh ];
+then
+  source ~/.nix-profile/etc/profile.d/nix.sh
+else
+  if [ -e /nix ] && [ -e $DMG ];
+    then
+      hdiutil attach $DMG -mountpoint /nix;
+      source ~/.nix-profile/etc/profile.d/nix.sh
+  fi
+fi
+" >> $RCFILE
 
 echo "Now logout and back in to use Nix, or source ~/.profile now."
